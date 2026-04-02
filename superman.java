dae51c30763886b1dee4f81c8a00762acc754f79
gbharatht <Jillagowtham02@gmail.com>
2026-04-02 11:15:03 +0530

    rollno=5,m1=67,m2=88,m3=98;        String name="batman";         total=m1+m2+m3;         System.out.println("\nStudent Details");         System.out.println("Rollno    "+rollno);         System.out.println("Name      "+name);         Syetem.out.println("Total     "+total);

--- superman.java
+++ superman.java
@@ -2,7 +2,7 @@ import java.io.*;
 class superman
 {
     public static void main (String[]args){
-        int rollno=5,m1=68,m2=88,m3=98;
+        int rollno=5,m1=67,m2=88,m3=98;
        String name="batman";
         total=m1+m2+m3;
         System.out.println("\nStudent Details");
