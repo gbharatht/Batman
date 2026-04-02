4b36c4756c5d14a1d5499532d5aa19286c717169
gbharatht <Jillagowtham02@gmail.com>
2026-04-02 10:58:19 +0530

    a 500 b 30

---
+++ superman.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class bat
+{
+    public static void main (String[]args){
+        int a,b,c,d;
+        a=500;
+        b=30;
+        c=a/b+b;
+        d=a*b-a;
+         System.out.ptintln("The divided Value is "+c);
+        System.out.ptintln("The Multiple Value is "+d);
+    } 
+}
