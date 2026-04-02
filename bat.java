b5b863b9229669363ae6f5781a3f3c0df64062a7
gbharatht <Jillagowtham02@gmail.com>
2026-04-02 10:22:19 +0530

    a 10 b 2

---
+++ bat.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class bat
+{
+    public static void main (String[]args){
+        int a,b,c;
+        a=10;
+        b=2;
+        c=a+b;
+        System.out.ptintln("The Total Value is "+c);
+    } 
+}
