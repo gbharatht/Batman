2231822494f491beefc38cb1b594b761ded11725
gbharatht <Jillagowtham02@gmail.com>
2026-04-02 10:34:37 +0530

    b 20

--- bat.java
+++ bat.java
@@ -4,7 +4,7 @@ class bat
     public static void main (String[]args){
         int a,b,c;
         a=10;
-        b=2;
+        b=20;
         c=a+b;
         System.out.ptintln("The Total Value is "+c);
     } 
